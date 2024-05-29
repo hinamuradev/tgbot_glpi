6de34bf652f336ec1543ece1b7695566fd11baf6
hinamuradev <149410901+hinamuradev@users.noreply.github.com>
2024-05-29 09:52:19 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,15 +1,1 @@
-import glpi_api
-
-URL = 'http://172.16.2.10/glpi/apirest.php/'
-APPTOKEN = '4Le7MHVAaFmslbZufNsmfrRdmkHbZMA9mWTmg9vH'
-USERTOKEN = 'YOURUSERTOKEN'
-
-try:
-    with glpi_api.connect(URL, APPTOKEN, USERTOKEN) as glpi:
-        print(glpi.get_config())
-except glpi_api.GLPIError as err:
-    print(str(err))
-
-glpi = glpi_api.GLPI(url=URL,
-                     apptoken=APPTOKEN,
-                     auth=('glpi', '%Avia407'))
+..
