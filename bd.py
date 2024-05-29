2d78bcc9e79fda92573421e13942750d399ef2e4
hinamuradev <149410901+hinamuradev@users.noreply.github.com>
2024-05-29 09:51:52 +0300

    Update bd.py
    v1.0

--- bd.py
+++ bd.py
@@ -1,34 +1,75 @@
+import html
+import time
+import requests
 from mysql.connector import connect, Error
 
-try:
-    with connect(
-        host="****",
-        user='***',
-        password='***',
-        database="*****",
-    ) as connection:
-        select_glpi_tickets_query = "SELECT name, date, content, users_id_recipient FROM glpi_tickets LIMIT 4"
-        with connection.cursor() as cursor:
-            cursor.execute(select_glpi_tickets_query)
-            result = cursor.fetchall()
-            print("Name     Date     Content    User_id")
-            for row in result:
-                print("%s    %s     %s    %s" % (row[0], row[1], row[2], row[3]))
-except Error as e:
-    print(e)
+# Параметры подключения к базе данных
+DATABASE_CONFIG = {
+    'host': '',
+    'user': '',
+    'password': '',
+    'database': '',
+}
 
-#
-# user_id_recipient_raw = [row[3] for row in result]    #user_id_recipient raw all
-# name_raw = [row[0] for row in result]     #name
-# date_raw = [row[1] for row in result]     #date
-# content_raw = [row[2] for row in result]    #content
-# user_id = str(user_id_recipient_raw[0])
-# name = str(name_raw[0])
-# date = str(date_raw[0])
-# content = str(content_raw[0])
-#
-# def output_tickets_in_bot():
-#     return "Поступила новая заявка.\nПользователь: " + user_id + "\n" + "Ее название " + name + "\nВремя: " + date + "\nОписание " + content
-#
-#
-# print(output_tickets_in_bot())
+# Токен вашего Telegram бота
+TOKEN = ''
+
+# Идентификатор вашего чата с ботом
+CHAT_ID = ''
+last_query = None
+
+
+def connect_to_database():
+    try:
+        connection = connect(**DATABASE_CONFIG)
+        return connection
+    except Error as e:
+        print("Ошибка подключения к базе данных:", e)
+        return None
+
+
+def execute_query(connection, query):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result
+    except Error as e:
+        print("Ошибка выполнения запроса:", e)
+        return None
+
+
+def send_telegram_message_to_group(message):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    params = {
+        'chat_id': '-1001259896915', 
+        'text': html.unescape(message)  # Удаление HTML-тегов из сообщения
+    }
+    response = requests.post(url, json=params)
+    if response.status_code != 200:
+        print("Ошибка отправки сообщения:", response.text)
+
+
+def check_for_new_queries():
+    global last_query
+
+    select_last_glpi_query = "SELECT name, date, content, users_id_recipient FROM glpi_tickets ORDER BY date DESC LIMIT 1"
+
+    connection = connect_to_database()
+    if connection:
+        result = execute_query(connection, select_last_glpi_query)
+        if result:
+            if result != last_query:
+                last_query = result
+                name, date, content, users_id_recipient = result[0]
+                contents = content.replace('&#60;p&#62;', '')
+                message = f"Обнаружен новый запрос в базе данных glpi_tickets!\nИмя: {name}\nДата: {date}\nСодержание: {contents}\nID пользователя: {users_id_recipient}"
+                send_telegram_message_to_group(message)
+        # Закрытие соединения с базой данных
+        connection.close()
+
+
+# Цикл для проверки новых запросов каждые 10 секунд
+while True:
+    check_for_new_queries()
+    time.sleep(10)
